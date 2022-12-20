3972e423ab0093998340ae8df42c5d5c14541905
Marco Inchingoli <evilmarkus@hotmail.it>
2022-12-20 18:04:27 +0100

    Add Milestone 1 Completed Trains Table on Homepage

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-
-        return view('trains');
+        $trains = Train::all();
+        /* dd($trains); */
+        return view('trains', compact('trains'));
     }
 
     public function about()
